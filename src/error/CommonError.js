6556620fe7ed8b8969af5935b986e02d6129e4c7
Jose Pleonasm <jose.pleonasm@gmail.com>
2025-01-26 19:07:03 +0100

    Better error

--- src/error/CommonError.js
+++ src/error/CommonError.js
@@ -1,1 +1,17 @@
-export class CommonError extends Error {}
+/**
+ * @typedef {Object} ErrorOptions
+ * @property {(Error | string)?} cause
+ */
+
+export class CommonError extends Error {
+	/**
+	 * @param {string} message
+	 * @param {(ErrorOptions | string)?} options
+	 * @param {string?} fileName
+	 * @param {string?} lineNumber
+	 */
+	constructor(message, options, fileName, lineNumber) {
+		const hasOptions = typeof options !== 'string';
+		super(message, hasOptions ? options : fileName, hasOptions ? fileName : lineNumber, lineNumber);
+	}
+}
--- src/error/InvalidParamError.js
+++ src/error/InvalidParamError.js
@@ -1,3 +1,9 @@
 import { CommonError } from './CommonError.js';
 
-export class InvalidParamError extends CommonError {}
+export class InvalidParamError extends CommonError {
+	constructor(paramName, options, fileName, lineNumber) {
+		super(`Invalid parameter "${paramName}".`, options, fileName, lineNumber);
+
+		this.paramName = paramName;
+	}
+}
