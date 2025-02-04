88e4c677384e8fea3972043775448fe3b265ff72
Jose Pleonasm <jose.pleonasm@gmail.com>
2025-02-04 19:02:42 +0100

    Add test

--- src/utils/math.test.js
+++ src/utils/math.test.js
@@ -4,3 +4,7 @@ import { add } from './math.js';
 test('adds 1 + 2 to equal 3', () => {
 	expect(add(1, 1, 2)).toBe(3);
 });
+
+test('should handle float numbers properly', () => {
+	expect(add(10000, 3.3, 2.9)).toBe(6.2);
+});
