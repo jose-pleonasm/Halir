d17d49ccb6db4239f123ee7f7d142b013fac3a02
Jose Pleonasm <jose.pleonasm@gmail.com>
2025-02-04 19:35:16 +0100

    propper testing kick-off

---
+++ src/test/getTestConfig.js
@@ -0,0 +1,29 @@
+import { Ticker } from '../cli/private/securitiesConfig.js'; // TODO
+
+export const getTestConfig = () => {
+	return {
+		// general
+		uuidNamespace: '8b558875-4377-477b-8d81-9b66ca9701b3',
+		numberScaleFactor: 10000,
+		lineSeparator: '\n',
+		columnSeparator: ',',
+		ticker: Ticker,
+
+		// input
+		fileEncoding: 'utf8',
+		columns:
+			'date,time,product,isin,exchangeReference,venue,quantity,price,priceCurrency,localValue,localValueCurrency,value,valueCurrency,exchangeRate,fees,feesCurrency,total,totalCurrency,orderId', // TODO: nebere se v potaz (na vstupu)
+		overviewColumns:
+			'isin,product,quantity,totalLocalValue,totalLocalValueCurrency,totalValue,totalValueCurrency,totalFees,totalFeesCurrency,total,totalCurrency',
+		locales: 'en',
+		timezone: 'UTC+01:00',
+		dateFormat: 'DD-MM-YYYY',
+		timeFormat: 'HH:MM',
+
+		// output
+		outputColumns: undefined,
+		outputLocales: 'cs-CZ',
+		outputTimezone: undefined,
+		outputColumnSeparator: '\t',
+	};
+};
