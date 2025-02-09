97bb526de68d370aa5c0d3c96d7390c6dda1d331
Jose Pleonasm <jose.pleonasm@gmail.com>
2025-02-09 18:34:40 +0100

    Better securities config

--- src/cli/private/securitiesConfig.js
+++ src/cli/private/securitiesConfig.js
@@ -1,77 +1,55 @@
-export const Symbol = {
+const SecurityMetadata = {
 	US67066G1040: {
-		default: 'NVDA',
+		symbol: { default: 'NVDA' },
+		exchange: { default: 'NASDAQ' },
 	},
 	US0079031078: {
-		default: 'AMD',
+		symbol: { default: 'AMD' },
+		exchange: { default: 'NASDAQ' },
 	},
 	US4581401001: {
-		default: 'INTC',
+		symbol: { default: 'INTC' },
+		exchange: { default: 'NASDAQ' },
 	},
 	US7475251036: {
-		default: 'QCOM',
+		symbol: { default: 'QCOM' },
+		exchange: { default: 'NASDAQ' },
 	},
 	US6821891057: {
-		default: 'ON',
+		symbol: { default: 'ON' },
+		exchange: { default: 'NASDAQ' },
 	},
 	GG00BBHX2H91: {
-		default: 'TRIG',
+		symbol: { default: 'TRIG' },
+		exchange: { default: 'LON' },
 	},
 	IE00B1XNHC34: {
-		default: 'INRG',
+		symbol: { default: 'INRG' },
+		exchange: { default: 'LON' },
 	},
 	GB0002374006: {
-		default: 'DGE',
+		symbol: { default: 'DGE' },
+		exchange: { default: 'LON' },
 	},
 	FR0000120693: {
-		default: 'RI',
+		symbol: { default: 'RI' },
+		exchange: { default: 'EPA' },
 	},
 	FR0013447729: {
-		default: 'VRLA',
+		symbol: { default: 'VRLA' },
+		exchange: { default: 'EPA' },
 	},
 	US88579Y1010: {
-		default: 'MMM',
+		symbol: { default: 'MMM' },
+		exchange: { default: 'NYSE' },
 	},
 	US83444M1018: {
-		default: 'SOLV',
+		symbol: { default: 'SOLV' },
+		exchange: { default: 'NYSE' },
 	},
 };
 
-export const Ticker = {
-	US67066G1040: {
-		default: `NASDAQ:${Symbol['US67066G1040'].default}`,
-	},
-	US0079031078: {
-		default: `NASDAQ:${Symbol['US0079031078'].default}`,
-	},
-	US4581401001: {
-		default: `NASDAQ:${Symbol['US4581401001'].default}`,
-	},
-	US7475251036: {
-		default: `NASDAQ:${Symbol['US7475251036'].default}`,
-	},
-	US6821891057: {
-		default: `NASDAQ:${Symbol['US6821891057'].default}`,
-	},
-	GG00BBHX2H91: {
-		default: `LON:${Symbol['GG00BBHX2H91'].default}`,
-	},
-	IE00B1XNHC34: {
-		default: `LON:${Symbol['IE00B1XNHC34'].default}`,
-	},
-	GB0002374006: {
-		default: `LON:${Symbol['GB0002374006'].default}`,
-	},
-	FR0000120693: {
-		default: `EPA:${Symbol['FR0000120693'].default}`,
-	},
-	FR0013447729: {
-		default: `EPA:${Symbol['FR0013447729'].default}`,
-	},
-	US88579Y1010: {
-		default: `NYSE:${Symbol['US88579Y1010'].default}`,
-	},
-	US83444M1018: {
-		default: `NYSE:${Symbol['US83444M1018'].default}`,
-	},
-};
+export const Ticker = Object.entries(SecurityMetadata).reduce(
+	(ticker, [isin, metadata]) => ({ ...ticker, [isin]: { default: `${metadata.exchange.default}:${metadata.symbol.default}` } }),
+	{},
+);
