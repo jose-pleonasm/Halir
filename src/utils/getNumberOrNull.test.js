3c3af95728cc4351ee277d7cc6501e69668a71b1
Jose Pleonasm <jose.pleonasm@gmail.com>
2025-10-18 12:25:47 +0200

    Add test for getNumberOrNull

---
+++ src/utils/getNumberOrNull.test.js
@@ -0,0 +1,27 @@
+import { describe, test, expect } from 'vitest';
+import { getNumberOrNull } from './getNumberOrNull';
+
+describe('getNumberOrNull', () => {
+	test.each([
+		['42', 42],
+		['3.14', 3.14],
+		['0', 0],
+		['-123.456', -123.456],
+		[null, null],
+		['', null],
+		['   ', NaN], // whitespace-only becomes NaN
+		['abc', NaN], // invalid numeric string
+		['123abc', 123], // parseFloat allows partial parsing
+		['0.00001', 0.00001],
+		['Infinity', Infinity],
+		['-Infinity', -Infinity],
+	])('getNumberOrNull(%j) -> %j', (input, expected) => {
+		const result = getNumberOrNull(input);
+
+		if (Number.isNaN(expected)) {
+			expect(result).toBeNaN();
+		} else {
+			expect(result).toBe(expected);
+		}
+	});
+});
